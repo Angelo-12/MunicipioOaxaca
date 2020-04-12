5eb72bad9b15c92bae1df24aefccd1cfd3bbc562
Angelo-12 <angel23.aj32@gmail.com>
2020-04-12 14:11:37 -0500

    rutas

--- app/Http/Controllers/Administrador/UsuarioController.php
+++ app/Http/Controllers/Administrador/UsuarioController.php
@@ -49,4 +49,88 @@ class UsuarioController extends Controller
       $usuario=User::all();
       return $usuario;
    }
+   public function iniciarsesion($id){
+      $usuario=Administrador_Secretaria::join('users','admin_secretaria.id','=','users.id')
+      ->join('municipio','users.id_municipio','=','municipio.id_municipio')
+      ->select('name','apellido_paterno','apellido_materno','email','cargo','nombre')
+      ->where('id_admin_secretaria','=',$id)
+      ->get();
+      return response()->json(['admin'=>$usuario]);
+
+   }
+   public function listarOrganizaciones(){
+      $organizacion=Organizacion::all();
+      $data=$organizacion->toArray();
+      return response(["Organizacion"=>$data]);
+      ;
+      
+   }
+   
+   public function listarzonascomerciales(){
+      $zonas=Zona::all();
+      $data=$zonas->toArray();
+      return response(["zonas"=>$data]);
+      ;
+      
+   }
+   public function listaractividadesbien(){
+      $actividades=Actividad::all();
+      $data=$actividades->toArray();
+      return response(["actividades"=>$data]);
+   }
+
+
+   public function listaractividades(){
+      $data=Permisos::select('tipo_actividad')
+      ->where('tipo_actividad','=','Ambulante')
+      ->count('tipo_actividad','=','Ambulante');
+      return response(["permisos"=>$data]);
+      ;
+      
+   }
+   public function listarvendedoresorganizacion($id){
+      $vendedores= Organizacion::join('vendedor','organizacion.id_organizacion','=','vendedor.id_organizacion')
+      ->join('users','vendedor.id','=','users.id')
+      ->join('permiso','vendedor.id_permiso','=','permiso.id_permiso')
+      ->join('calle','permiso.id_calle','=','calle.id_calle')
+      ->join('zona','calle.id_zona','=','zona.id_zona')
+      ->select('id_vendedor','name','apellido_paterno','apellido_materno','nombre_organizacion','tipo_actividad','giro','latitud','longitud','zona.nombre')
+      ->where('organizacion.id_organizacion','=',$id)
+      ->get();
+      return response(["permisos"=>$vendedores]);
+      ;  
+   }
+   public function listarvendedoreszonas($id){
+      $vendedores=Zona::join('calle','zona.id_zona','=','calle.id_zona')
+      ->join('permiso','calle.id_calle','=','permiso.id_calle')
+      ->join('vendedor','permiso.id_permiso','=','vendedor.id_permiso')
+      ->join('organizacion','vendedor.id_organizacion','=','organizacion.id_organizacion')
+      ->join('users','vendedor.id','=','users.id')
+      ->select('zona.id_zona','vendedor.id_vendedor','name','apellido_paterno','apellido_materno','nombre_organizacion','tipo_actividad','giro','latitud','longitud','zona.nombre')
+      ->where('zona.id_zona','=',$id)
+      ->get();
+      return response(["zonasvend"=>$vendedores]);
+   }
+
+   public function listarvendedores(){
+      $vendedores= Vendedor::join('users','vendedor.id','=','users.id')
+      ->join('organizacion','vendedor.id_organizacion','=','organizacion.id_organizacion')
+      ->join('permiso','vendedor.id_permiso','=','permiso.id_permiso')
+      ->join('calle','permiso.id_calle','=','calle.id_calle')
+      ->join('zona','calle.id_zona','=','zona.id_zona')
+      ->select('id_vendedor','name','apellido_paterno','apellido_materno','nombre_organizacion','tipo_actividad','giro','latitud','longitud','zona.nombre')
+      ->orderby('id_vendedor')
+      ->get();
+      return response(["vendedores"=>$vendedores]);
+      ;
+      
+   }
+   
+   public function ver_detalle($id){
+		
+      $vendedores=Administrador_Secretaria::join('users','admin_secretaria.id','=','users.id')
+      ->select('name','apellido_paterno','apellido_materno','email','cargo')->get();
+      return response()->json(['admin'=>$usuario]);
+      
+	}
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,16 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('crearUsuario','Administrador\UsuarioController@crearUsuario');
+
+Route::group(['prefix'=>'Usuario'],function(){
+    Route::get('listar','Administrador\UsuarioController@listarUsuarios');
+    Route::get('iniciar/{nombre}','Administrador\UsuarioController@iniciarsesion');
+    Route::get('listarorg','Administrador\UsuarioController@listarOrganizaciones');
+    Route::get('deta/{nombre}','Administrador\UsuarioController@listarvendedoresorganizacion');
+    Route::get('listarzona','Administrador\UsuarioController@listarzonascomerciales');
+    Route::get('vendedoreslist','Administrador\UsuarioController@listarvendedores');
+    Route::get('listarzonavendedor/{nombre}','Administrador\UsuarioController@listarvendedoreszonas');
+    Route::get('listaractividades','Administrador\UsuarioController@listaractividadesbien');
+});
